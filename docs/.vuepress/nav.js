c7e91ae53bd4e3a9f16951d29f92bb0b68fe1899
kevin <1368052450@qq.com>
2023-08-14 20:48:08 +0800

    初始化项目

--- docs/.vuepress/nav.js
+++ docs/.vuepress/nav.js
@@ -1,26 +1,27 @@
 module.exports = [
   { text: '首页', link: '/' },
+  // {
+  //   text: '前端',
+  //   link: '/web/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
+  //   items: [
+  //     // 说明：以下所有link的值只是在相应md文件头部定义的永久链接（不是什么特殊编码）。另外，注意结尾是有斜杠的
+  //     {
+  //       text: '前端文章',
+  //       items: [
+  //         { text: 'JavaScript', link: '/pages/8143cc480faf9a11/' },
+  //       ],
+  //     },
+
+  //   ]
+  // },
   {
     text: '前端',
-    link: '/web/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
+    link: '/front/',
     items: [
-      // 说明：以下所有link的值只是在相应md文件头部定义的永久链接（不是什么特殊编码）。另外，注意结尾是有斜杠的
-      {
-        text: '前端文章',
-        items: [
-          { text: 'JavaScript', link: '/pages/8143cc480faf9a11/' },
-        ],
-      },
-
-    ]
-  },
-  {
-    text: '页面',
-    link: '/ui/',
-    items: [
-      { text: 'HTML', link: '/pages/c1cd0d/' },
-      { text: 'CSS', link: '/pages/904a4c/' },
-      { text: 'git', link: '/note/git/' }
+      { text: 'HTML', link: '/note/HTML/' },
+      { text: 'CSS', link: '/note/CSS/' },
+      { text: 'Git', link: '/note/Git/' },
+      { text: 'Webpack', link: '/note/Webpack/' }
     ],
   },
   {
